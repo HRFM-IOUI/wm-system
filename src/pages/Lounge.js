0caa6271741750747f2056d4d223a747112b3e1a
i know <ik39.10vevic@gmail.com>
2025-03-24 11:41:51 +0900

    LoungeページをReactで作成

---
+++ src/pages/Lounge.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Lounge.css';
+
+export default function Lounge() {
+    return (
+        <div className="lounge-container">
+            <h1>TOA-FANS.SHOP</h1>
+            <p>ここにElementorから出力した内容を忠実に再現する。</p>
+            {/* 後ほど見本!DOCTYPE.htmlから移植する */}
+        </div>
+    );
+}
