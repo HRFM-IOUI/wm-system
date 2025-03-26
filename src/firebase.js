ad1809ab2bcb0d72bca8118ba0f0629f183d0359
i know <ik39.10vevic@gmail.com>
2025-03-27 02:28:41 +0900

    Gacha UI update

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcw0yOOCIG8E1UiXwK5quT4XnE14jIz14",
-  authDomain: "wmfansystem.firebaseapp.com",
-  projectId: "wmfansystem",
-  storageBucket: "wmfansystem.appspot.com",
-  messagingSenderId: "494534456119",
-  appId: "1:494534456119:web:4bd67d6d2fa6a1a36de0b2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Firebase アプリの初期化（重複回避）
